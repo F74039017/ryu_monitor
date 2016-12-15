c81aeac508bb313311f316f418ae16880c407489
nckuutc <utcncku.gmail.com>
2016-12-16 00:28:39 +0800

    Update website 00:28

--- gui_topology/html/js/main_navbar.js
+++ gui_topology/html/js/main_navbar.js
@@ -1,22 +1,27 @@
 $(document).ready(function() {
     $('#icon-info').click(function() {
-        $('#tab-info').animate({ "right": "0px" }, 500).addClass('visible');
+        $('#tab-info').animate({ "right": "0px" }, 500).removeClass('hidden').addClass('visible');
     });
 
     $('#info-back').click(function() {
+    	console.log('tab-info: ' + $('#tab-info').hasClass('visible'));	//test
         if ($('#tab-info').hasClass("visible")) {
-            $('#tab-info').animate({ "right": "-1000px" }, 500).removeClass('visible');
+            $('#tab-info').animate({ "right": "-1000px" }, 500).removeClass('visible').addClass('hidden');
+        } else {
+            $('#tab-info').animate({ "right": "-1000px" }, 500).removeClass('hidden').addClass('visible');
         }
     });
 
     $('#icon-topo').click(function() {
-        $('#tab-topo').animate({ "right": "0px" }, 500).addClass('visible');
+        $('#tab-topo').animate({ "right": "0px" }, 500).removeClass('hidden').addClass('visible');
     });
 
     $('#topo-back').click(function() {
-        console.log($('#tab-topo').hasClass('visible'));
+        console.log('tab-topo: ' + $('#tab-topo').hasClass('visible'));	//test
         if ($('#tab-topo').hasClass("visible")) {
-            $('#tab-topo').animate({ "right": "-1000px" }, 500).removeClass('visible');
+            $('#tab-topo').animate({ "right": "-1000px" }, 500).removeClass('visible').addClass('hidden');
+        } else {
+            $('#tab-topo').animate({ "right": "-1000px" }, 500).removeClass('hidden').addClass('visible');
         }
     });
 });
